bd47b1cc2e4e4be86b809f7ec59f10d6de37311f
Florian Oswald <florian.oswald@gmail.com>
2015-04-24 16:23:59 +0100

    working version

--- scraper.py
+++ scraper.py
@@ -3,13 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from codecs import open
 import os
 import json
-from datetime import datetime
+import copy
+from datetime import datetime, timedelta
 
 
 # http://stackoverflow.com/questions/989872/how-do-i-draw-out-specific-data-from-an-opened-url-in-python-using-urllib2/989920#989920
@@ -19,20 +20,21 @@ from datetime import datetime
 
 
 class Driver:
-    def __init__(self, url):
+    def __init__(self, url, wait=30):
         self.baseurl = url
         self.driver = webdriver.Firefox()
         self.driver.get(url)
+        self.wait = wait
 
     def goSearch(self):
         if self.getURL() is not self.baseurl:
             self.goHome()
             try: 
-                WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.LINK_TEXT, "District Civil/Criminal Records")))
+                WebDriverWait(self.driver, self.wait).until(EC.presence_of_element_located((By.LINK_TEXT, "District Civil/Criminal Records")))
                 self.driver.find_element_by_link_text("District Civil/Criminal Records").click()
             except NoSuchElementException:
-                print "server is done after 30 secs. restart."
-                self.driver.implicitly_wait(5)
+                print "server is hung after %d secs. restart. please wait." % self.wait
+                self.driver.implicitly_wait(self.wait)
                 self.goHome()
                 self.driver.refresh()
                 self.goSearch()
@@ -48,89 +50,23 @@ class Driver:
 
 
 class Bank:
-    def __init__(self, bname, Driver):
+    def __init__(self, bname, Driver, dates,verbose=0):
         self.bankname = bname
         self.browser = Driver
-        self.dates = ['no from', 'no to']
         self.currentCase = 'none'
         self.cases = {}
         self.casesToGo = {}
-        self.done = False
+        self.done = False 	# current search over subset of date range done
+        self.doneRange = False	# search over entire date range done: get new date range, or exit
         self.searchURL = 'none'
         self.fname = 'none'
         self.changeDates = True
-        self.data = {'bank': bname, 'caseCount': 0, 'done': False, 'dates_from': "0", "dates_to": "0",
-                     'currentCase': 'none'}
-
-    def submitSearch(self, dates):
-        """ submits a search query for bank
-		at dates. browser will display search results"""
-
         assert len(dates) == 2
-        self.dates[0] = dates[0]
-        self.dates[1] = dates[1]
-        self.data["dates_from"] = self.dates[0]
-        self.data["dates_to"] = self.dates[1]
+        self.dates = dates
+        self.submittedDates = copy.deepcopy(dates)
+        self.data = {'bank': bname, 'caseCount': 0, 'done': False, 'dates_from': "0", "dates_to": "0",
+                     'currentCase': 'none', 'cases' : {}}
 
-        # move browser to district court search
-        # if self.browser.driver.current_url is self.browser.baseurl:
-        self.browser.goSearch()
-
-        try:
-            downelt = WebDriverWait(self.browser.driver, 30).until(
-                EC.presence_of_element_located((By.XPATH, "//select[@id='SearchBy']")))
-            ddown = Select(downelt)
-        except ElementNotVisibleException:
-            print "waited for 30 seconds to load search page. quitting driver."
-            self.browser.driver.quit()
-
-        ddown.select_by_visible_text("Party")
-        # click on radio button "Business"
-        self.browser.driver.find_element_by_xpath("//input[@id='PartyBusinessOption']").click()
-        # fill in name of bank we are looking for
-        self.browser.driver.find_element_by_xpath("//input[@id='LastName']").send_keys(self.bankname)
-        # could specify dates here:
-        self.browser.driver.find_element_by_xpath("//input[@id='DateFiledOnAfter']").send_keys(self.dates[0])
-        self.browser.driver.find_element_by_xpath("//input[@id='DateFiledOnBefore']").send_keys(self.dates[1])
-        # submit the search
-
-        self.browser.driver.find_element_by_xpath("//input[@id='SearchSubmit']").click()
-        self.browser.driver.implicitly_wait(2)
-
-        # store the URL of search results: list of cases
-        self.searchURL = self.browser.driver.current_url
-
-    def checkSearch(self):
-        if self.browser.driver.current_url is not self.searchURL:
-            self.browser.goSearch()
-            print "checkSearch has reset the browser to search district courts"
-            return True
-        else:
-            return False
-
-    def reSubmitSearch(self):
-        # move browser to district court search
-        self.browser.goSearch()
-
-        try:
-            downelt = WebDriverWait(self.browser.driver, 15).until(
-                EC.presence_of_element_located((By.XPATH, "//select[@id='SearchBy']")))
-            ddown = Select(downelt)
-        except ElementNotVisibleException:
-            print "waited for 15 seconds to load search page. quitting driver."
-            self.browser.driver.quit()
-        ddown.select_by_visible_text("Party")
-        # click on radio button "Business"
-        self.browser.driver.find_element_by_xpath("//input[@id='PartyBusinessOption']").click()
-        # fill in name of bank we are looking for
-        self.browser.driver.find_element_by_xpath("//input[@id='LastName']").send_keys(self.bankname)
-        # could specify dates here:
-        self.browser.driver.find_element_by_xpath("//input[@id='DateFiledOnAfter']").send_keys(self.dates[0])
-        self.browser.driver.find_element_by_xpath("//input[@id='DateFiledOnBefore']").send_keys(self.dates[1])
-        # submit the search
-        self.browser.driver.find_element_by_xpath("//input[@id='SearchSubmit']").click()
-        # store the URL of search results: list of cases
-        self.searchURL = self.browser.driver.current_url
 
     def checkCaseList(self):
         soup = BeautifulSoup(self.browser.driver.page_source)
@@ -151,6 +87,25 @@ class Bank:
             self.changeDates = False
 
 
+    def setCases(self, cases):
+        self.cases = cases
+        self.casesToGo = cases
+        self.currentCase = cases.keys()[0]
+
+    def caseDone(self):
+        self.casesToGo.pop(self.currentCase)  # get rid of current case
+        if len(self.casesToGo) == 0 and self.dates[1] == self.submittedDates[1] :
+            self.done = True
+            self.doneRange = True
+        elif len(self.casesToGo) == 0 and self.dates[1] != self.submittedDates[1]:
+            self.done = True
+            self.doneRange = False
+        else:
+            self.currentCase = self.casesToGo.keys()[0]  # replace current case and go on
+
+
+    # parsing functions
+    # =================
 
     def parseCaseList(self, term):
         """ get the HTML source of the current page_source
@@ -171,17 +126,7 @@ class Bank:
         self.setCases(BoCcases)
 
 
-    def setCases(self, cases):
-        self.cases = cases
-        self.casesToGo = cases
-        self.currentCase = cases.keys()[0]
 
-    def caseDone(self):
-        self.casesToGo.pop(self.currentCase)  # get rid of current case
-        if len(self.casesToGo) == 0:
-            self.done = True
-        else:
-            self.currentCase = self.casesToGo.keys()[0]  # replace current case
 
 
     def parseSingleCase(self):
@@ -195,7 +140,7 @@ class Bank:
         # get URL of this case. useless as far as I can see.
         d["URL"] = self.browser.driver.current_url.encode('utf-8')
 
-        print "parsing URL %s" % d["URL"]
+        # print "parsing URL %s" % d["URL"]
 
         # get all table headers	of that class:
         h = soup.find_all(class_="ssTableHeaderLabel")
@@ -203,7 +148,10 @@ class Bank:
         # ROA: register of actions
         # get case number
         roa = soup.find(class_="ssCaseDetailCaseNbr")
-        d[roa.contents[0].strip().encode('utf-8')] = roa.contents[1].get_text().encode('utf-8')
+        caseid = roa.contents[1].get_text().encode('utf-8')
+        #add ID prefix to case id?
+        #d[roa.contents[0].strip().encode('utf-8')] = ''.join(['ID_',caseid])
+        d[roa.contents[0].strip().encode('utf-8')] = caseid
 
         # get all other data from top right table
         # type of case
@@ -261,12 +209,17 @@ class Bank:
         # print "in case number %s" % d["Case No."]
 
         # save data into self.data
-        self.data[self.currentCase] = d
+        self.data['cases'][self.currentCase] = d
 
 
-    def startBankSearch(self, dates):
-        self.submitSearch(dates)
+
+    # Submit Search functions
+    # =======================
+
+    def startBankSearch(self):
+        self.submitSearch()
         self.caseCount = 0
+        print "started search from %s to %s" % (self.dates[0],self.dates[1])
         try:
             WebDriverWait(self.browser.driver, 15).until(EC.presence_of_element_located(
                 (By.XPATH, "/html/body/table[4]/tbody/tr[1]/th[1][@class='ssSearchResultHeader']/b")))
@@ -292,7 +245,9 @@ class Bank:
         # run the search
         self.data["dates_from"] = self.dates[0]
         self.data["dates_to"] = self.dates[1]
-        self.continueBankSearch()
+        retval = self.continueBankSearch()
+        return retval
+
 
     # def warmStart(file):
     	# load file
@@ -302,68 +257,174 @@ class Bank:
     	# set self.currentCase = data["currentCase"]
     	# call 
 
+    def submitSearch(self):
+        """ submits a search query for bank
+		at dates. browser will display search results"""
+
+        # assert len(dates) == 2
+        # self.dates[0] = dates[0]
+        # self.dates[1] = dates[1]
+
+        # store submitted dates so I know if have to continue or if this is last batch
+        # self.submittedDates = dates
+
+        # move browser to district court search
+        # if self.browser.driver.current_url is self.browser.baseurl:
+        self.browser.goSearch()
+
+        try:
+            downelt = WebDriverWait(self.browser.driver, self.browser.wait).until(
+                EC.presence_of_element_located((By.XPATH, "//select[@id='SearchBy']")))
+            ddown = Select(downelt)
+        except ElementNotVisibleException:
+            print "waited for %d seconds to load search page. quitting driver." % self.browser.wait
+            self.browser.driver.quit()
+
+        ddown.select_by_visible_text("Party")
+        # click on radio button "Business"
+        self.browser.driver.find_element_by_xpath("//input[@id='PartyBusinessOption']").click()
+        # fill in name of bank we are looking for
+        self.browser.driver.find_element_by_xpath("//input[@id='LastName']").send_keys(self.bankname)
+        # could specify dates here:
+        self.browser.driver.find_element_by_xpath("//input[@id='DateFiledOnAfter']").send_keys(self.dates[0])
+        self.browser.driver.find_element_by_xpath("//input[@id='DateFiledOnBefore']").send_keys(self.dates[1])
+        # submit the search
+
+        self.browser.driver.find_element_by_xpath("//input[@id='SearchSubmit']").click()
+        self.browser.driver.implicitly_wait(2)
+
+        # store the URL of search results: list of cases
+        self.searchURL = self.browser.driver.current_url
+
+    def checkSearch(self):
+        if self.browser.driver.current_url is not self.searchURL:
+            self.browser.goSearch()
+            print "checkSearch has reset the browser to search district courts"
+            return True
+        else:
+            return False
+
+    def reSubmitSearch(self):
+        # move browser to district court search
+        self.browser.goSearch()
+
+        try:
+            downelt = WebDriverWait(self.browser.driver, self.browser.wait).until(
+                EC.presence_of_element_located((By.XPATH, "//select[@id='SearchBy']")))
+            ddown = Select(downelt)
+        except ElementNotVisibleException:
+            print "waited for %d seconds to load search page. quitting driver." % self.browser.wait
+            self.browser.driver.quit()
+        ddown.select_by_visible_text("Party")
+        # click on radio button "Business"
+        self.browser.driver.find_element_by_xpath("//input[@id='PartyBusinessOption']").click()
+        # fill in name of bank we are looking for
+        self.browser.driver.find_element_by_xpath("//input[@id='LastName']").send_keys(self.bankname)
+        # could specify dates here:
+        self.browser.driver.find_element_by_xpath("//input[@id='DateFiledOnAfter']").send_keys(self.dates[0])
+        self.browser.driver.find_element_by_xpath("//input[@id='DateFiledOnBefore']").send_keys(self.dates[1])
+        # submit the search
+        self.browser.driver.find_element_by_xpath("//input[@id='SearchSubmit']").click()
+        # store the URL of search results: list of cases
+        self.searchURL = self.browser.driver.current_url
 
     def continueBankSearch(self):
 
-        while self.done is not True:
+        while self.doneRange is False:
 
-            # check if browser is on search results page
-            # if self.checkSearch():
-            #     self.reSubmitSearch()
+            while self.done is False:
 
-            case = self.currentCase
-            self.caseCount = self.caseCount + 1
-            print "trying case %s" % case
-            try:
-                self.browser.driver.find_element_by_link_text(case).click()  #click on the required link to access a single case
-                self.parseSingleCase()  # get individual case data
-                self.browser.driver.back()  #  bring browser back on page
-                self.browser.driver.implicitly_wait(1)  # and wait 1 second
-            except NoSuchElementException:
-                print "could not open link for case %s" % case
-                print "trying to reset the browser"
-                self.browser.goHome()
-                self.browser.driver.implicitly_wait(10)  # and wait 1 second
-                self.browser.goSearch()
-                self.reSubmitSearch()
-                self.browser.driver.implicitly_wait(1)  # and wait 1 second
-                pass
-            except AttributeError:
-                print "could not parse case %s" % case
-                pass
+                case = self.currentCase
+                self.caseCount = self.caseCount + 1
+                # print "trying case %s" % case
+                try:
+                    link = WebDriverWait(self.browser.driver,self.browser.wait).until(EC.element_to_be_clickable((By.LINK_TEXT,case)))
+                    link.click()  #click on the required link to access a single case
+                    self.parseSingleCase()  # get individual case data
+                    self.caseDone()
+                    self.browser.driver.back()  #  bring browser back on page
+                except TimeoutException:
+                    # print "could not open link for case %s" % case
+                    # print "trying to reset the browser"
+                    self.data['cases'][self.currentCase] = { "hasData" : False }
+                    self.caseDone()
+                    self.browser.goHome()
+                    self.browser.goSearch()
+                    self.reSubmitSearch()
+                    pass
+                except AttributeError:
+                    print "could not parse case %s" % case
+                    pass
 
-            self.data['caseCount'] = self.caseCount
-            self.data['currentCase'] = case
+                self.data['caseCount'] = self.caseCount
+                self.data['currentCase'] = case
 
-            if self.caseCount % 10 is 0:
+                if self.caseCount % 20 is 0:
+                    self.updateData()
+
+            if self.doneRange is False:
+                # get new date range
+                # get final date of last search:
+                todate = datetime.strptime(self.dates[1],"%m/%d/%Y")
+                newfromdate = todate + timedelta(days = 1)
+                self.dates[0] = newfromdate.strftime("%m/%d/%Y")
+                self.dates[1] = self.submittedDates[1]
+                self.done = False
+                print "submitting search for %s - %s" % (self.dates[0],self.submittedDates[1])
+                self.startBankSearch()
+            else:
+                print "finished search in %s" % self.bankname
                 self.updateData()
 
-            # tick off this case as done
-            self.caseDone()
+        # return control to upper loop
+        return 0
+
 
     def updateData(self):
         f = open(self.fname, 'w')
         json.dump(self.data, f)
         f.close()
-        print "saved data to %s" % self.fname
+        print "saved data at case count %d to file %s" % (self.caseCount,self.fname)
+
         #     print "created %s" % self.fname
         # else:
-        #     print "trying to update %s" % self.fname
+        #     print "trying to update %s" % self.as
         #     f = open(self.fname, 'w')
         #     d = json.load(f)
         #     d.update(self.data)
         #     json.dump(d,f)
         #     f.close()
 
+
+# global functions
+
+def parse_string(el):
+   text = ''.join(el.findAll(text=True))
+   return text.strip()
+
 # run the scraper:
 
-banks = ["WELLS FARGO BANK", "US BANK"]
-# banks =["WELLS FARGO BANK","US BANK","CITIBANK","DEUTSCHE BANK NA TRUS","BAC HOME LOAN SERVICI","JP MORTGAGE CHASE BANK","CHASE HOME FINANCE","HSBC ","BANK OF AMERICA","GMAC ","PNC NATIONAL BANK","BANK OF NEW YORK MELL","NATIONAL CITY BANK","AURORA LOAN SERVICES","LASALLE BANK NA","PROVIDIAN NATIONAL BANK","NEVADA STATE BANK","WASHINGTON MUTUAL","CAPITAL ONE","ONE NEVADA CREDIT UNION","AMERICA FIRST CREDIT UNION","CLARK COUNTY CREDIT UNION","WEST STAR CREDIT UNION","PLUS CREDIT UNION","STAGE EMPLOYEES FEDERAL CREDIT UNION"]
-dates = [("01/01/2000", "01/01/2008"), ("01/01/2008", "01/01/2015")]
+
+def run():
+	banks = ["WELLS FARGO BANK", "US BANK","CITIBANK","DEUTSCHE BANK NA TRUS","BAC HOME LOAN SERVICI","JP MORTGAGE CHASE BANK","CHASE HOME FINANCE","HSBC ","BANK OF AMERICA","GMAC ","PNC NATIONAL BANK","BANK OF NEW YORK MELL"]
+	# banks = ["US BANK"]
+	# banks =["WELLS FARGO BANK","US BANK","CITIBANK","DEUTSCHE BANK NA TRUS","BAC HOME LOAN SERVICI","JP MORTGAGE CHASE BANK","CHASE HOME FINANCE","HSBC ","BANK OF AMERICA","GMAC ","PNC NATIONAL BANK","BANK OF NEW YORK MELL","NATIONAL CITY BANK","AURORA LOAN SERVICES","LASALLE BANK NA","PROVIDIAN NATIONAL BANK","NEVADA STATE BANK","WASHINGTON MUTUAL","CAPITAL ONE","ONE NEVADA CREDIT UNION","AMERICA FIRST CREDIT UNION","CLARK COUNTY CREDIT UNION","WEST STAR CREDIT UNION","PLUS CREDIT UNION","STAGE EMPLOYEES FEDERAL CREDIT UNION"]
+	# provide 2 time spans by default
+	dateSpan = ["01/01/2000", "01/01/2015"]
+	dr = Driver("https://www.clarkcountycourts.us/Anonymous/default.aspx",30)
+	for b in banks:
+		print "starting to search: %s" % b
+		print "==========================="
+		print ""
+		bb = Bank(b, dr, dateSpan)
+		retval = bb.startBankSearch()
+
+	print "scraper finished with:"
+	print banks
+	print "exiting."
+	dr.driver.quit()
+	return 0
 
 if __name__ == "__main__":
-    d = Driver("https://www.clarkcountycourts.us/Anonymous/default.aspx")
-    for b in banks:
-        bb = Bank(b, d)
-        bb.startBankSearch(dates[0])
+	run()
 
